34b708a6b1570227e530758ed048f60b1bb56a03
Aditya Kela <kelaaditya@gmail.com>
2017-03-23 18:58:40 +0100

    Added report hook for urlretrieve

--- style_transfer.py
+++ style_transfer.py
@@ -15,6 +15,16 @@ def download_vgg(link, file_name):
     Checks if downloaded previously
     '''
     
+    def report_hook(block_num, block_size, total_size):
+        '''A report hook for urlretrieve
+        '''
+        current_size = block_num * block_size
+        if total_size > 0:
+            percent_size = current_size / total_size * 100
+            progress_string = '{0}, {1} out of {2}\n'.format(percent_size, current_size, total_size)
+            if percent_size % 10 == 0:
+                sys.stdout.write(progress_string)
+    
     if os.path.exists(file_name) and os.stat(file_name).st_size == 534904783
         print('VGGNet ready')
     else:
