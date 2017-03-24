fbdeac492bd574a641b6fa73ca53dac0e646b001
Aditya Kela <kelaaditya@gmail.com>
2017-03-24 13:58:54 +0100

    Added stylize function

--- stylize.py
+++ stylize.py
@@ -75,3 +75,42 @@ def combined_loss_function(graph, input_image, style_image, content_image, conte
     return(content_loss_value, style_loss_value, total_loss_value)
 
 
+def stylize(content_image, 
+            style_image, 
+            content_weight, 
+            style_weight, 
+            initial_image_noise_ratio, 
+            pooling_func='avg'):
+    
+    content_image_width, content_image_height = content_image.size
+    
+    download_vgg.download_vgg(VGG_LINK, VGG)
+    
+    input_image = tf.Variable(np.zeros([1, content_image_height, content_image_width, 3]), dtype=tf.float32)
+    graph = vgg.load_vgg('./imagenet-vgg-verydeep-19.mat', input_image, pooling_func)
+    
+    content_image = content_image - MEAN_PIXELS
+    
+    style_image = style_image - MEAN_PIXELS
+    
+    content_loss, style_loss, total_loss = combined_loss_function(graph, 
+                                                                  input_image, 
+                                                                  style_image, 
+                                                                  content_image, 
+                                                                  content_weight, 
+                                                                  style_weight)
+    
+    optimizer = tf.train.AdamOptimizer(1.0).minimize(total_loss)
+    
+    initial_image = noisy_image(content_image, content_image_height, content_image_width, initial_image_noise_ratio)
+    
+    for iterations in range(100):
+        with tf.Session() as sess:
+            sess.run(tf.global_variables_initializer())
+            sess.run(input_image.assign(initial_image))
+            sess.run(optimizer)
+            generated_image, total_loss_value = sess.run([initial_image, total_loss])
+            generated_image = generated_image + MEAN_PIXELS
+            
+    final_image = np.clip(generated_image[0], 0, 255).astype('uint8')
+    scipy.misc.imsave('./neural_styled/', final_image)
