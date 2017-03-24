a8e45c8dfab049fc0238fbaff237f910b687ddbc
Aditya Kela <kelaaditya@gmail.com>
2017-03-24 13:55:18 +0100

    Added MEAN_PIXEL based on GithubGist

--- stylize.py
+++ stylize.py
@@ -4,3 +4,19 @@ import download_vgg
 import vgg
 
 from PIL import Image, ImageOps
+
+
+VGG_LINK = 'http://www.vlfeat.org/matconvnet/models/imagenet-vgg-verydeep-19.mat'
+VGG = 'imagenet-vgg-verydeep-19.mat'
+
+STYLE_LAYERS = ('relu1_1', 'relu2_1', 'relu3_1', 'relu4_1', 'relu5_1')
+CONTENT_LAYER = 'relu4_2'
+
+'''See GitHub gist: https://gist.github.com/ksimonyan/211839e770f7b538e2d8
+"The input images should be zero-centered by mean pixel (rather than mean image) subtraction. 
+Namely, the following BGR values should be subtracted: [103.939, 116.779, 123.68]."
+'''
+MEAN_PIXELS = np.array([123.68, 116.779, 103.939]).reshape((1,1,1,3))
+
+#The deeper the layer, the higher the weight
+LAYER_WEIGHTS = (0.5, 1.0, 1.5, 3.0, 4.0)
