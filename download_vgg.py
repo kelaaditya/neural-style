db6871ad16258884acf480333f776d055740ed0a
Aditya Kela <kelaaditya@gmail.com>
2018-10-03 16:55:10 +0530

    added progress bar to download

--- download_vgg.py
+++ download_vgg.py
@@ -1,33 +1,40 @@
 import os
 import sys
-import urllib.request
+from tqdm import tqdm
+import requests
 
 
 def download_vgg(link, file_name):
     '''Download pre-trained VGGNet
     Checks if downloaded previously
     '''
-    
-    def report_hook(block_num, block_size, total_size):
-        '''A report hook for urlretrieve
-        '''
-        current_size = block_num * block_size
-        if total_size > 0:
-            percent_size = current_size / total_size * 100
-            progress_string = '{0}, {1} out of {2}\n'.format(percent_size, current_size, total_size)
 
-            #TODO: A better counter for printing
-            #if percent_size % 10 == 0:
-            #    sys.stdout.write(progress_string)
+    base_folder = os.path.dirname(__file__)
+    file_name = os.path.join(base_folder, file_name)
+    file_size = 534904783
     
-    if os.path.exists(file_name) and os.stat(file_name).st_size == 534904783:
+    if os.path.exists(file_name) and os.stat(file_name).st_size == file_size:
         print('VGGNet ready')
     else:
-        print('Downloading VGG')
-        urllib.request.urlretrieve(link, file_name, report_hook)
+        print('Downloading VGG... ')
+
+        response = requests.get(link, stream=True)
+        response.raise_for_status()
+
+        block_size = 32 * 1024
+        with open(file_name, 'wb') as data_file:
+            with tqdm(total=file_size, unit='B', unit_scale=True, unit_divisor=1024) as pbar:
+                for chunk in response.iter_content(chunk_size=block_size):
+                    if chunk:
+                        data_file.write(chunk)
+                        pbar.update(len(chunk))
+        print(f'{file_name} download complete.')
 
 
 if __name__=="__main__":
     vgg_link = 'http://www.vlfeat.org/matconvnet/models/imagenet-vgg-verydeep-19.mat'
     vgg_filename = 'imagenet-vgg-verydeep-19.mat'
-    download_vgg(vgg_link, vgg_filename)
+    try:
+        download_vgg(vgg_link, vgg_filename)
+    except Exception as e:
+        print(e)
